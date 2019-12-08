9d50b354f5e48ef9d8d793417064c2b792c9acf3
raj-varshith <57326825+raj-varshith@users.noreply.github.com>
2019-12-08 17:14:15 +0530

    Add files via upload

---
+++ asgn1_3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a;
+    scanf("%c",&a);
+  if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U'){
+            printf("VOWEL");
+        }
+      else{
+        printf("CONSONANT");
+    }
+   return 0;
+}
